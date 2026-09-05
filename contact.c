9f61c11dab5528751e2facd22d63b2dd948bbacb
vijaypailwanwale <121452332+vijaypailwanwale@users.noreply.github.com>
2026-09-05 07:32:38 +0000

    Serch function updated

--- contact.c
+++ contact.c
@@ -190,19 +190,32 @@ void createContact(AddressBook *create)
 
 }
 
-void search_disp_name(AddressBook *book, char string[],int size)
+void search_disp(AddressBook *book, char string[],int size,int op)
 {
     int fount=0;
+
     printf("-----------------------------------------------");
     printf("\n|%5s|%20s |%12s| |%30s|\n", "Sr.no","Name", "Phone", "Email");
     printf("-----------------------------------------------");
 
     for(int i = 0; i < book->contactCount; i++)
     {
-        if(strstr(book->contacts.name[i],string) != NULL)
-        {
+        if(op == 1){
+        
+            if(strstr(book->contacts.name[i],string) != NULL)
+            {
             printf("|%5.4d| |%20.20s| |%12.12s| |%30.30s|\n", i + 1, book->contacts[i].name, book->contacts[i].phone, book->contacts[i].email);
             found = 1;
+            }
+        }
+
+            else if(op == 2){
+        
+            if(strstr(book->contacts.phone[i],string) != NULL)
+            {
+            printf("|%5.4d| |%20.20s| |%12.12s| |%30.30s|\n", i + 1, book->contacts[i].name, book->contacts[i].phone, book->contacts[i].email);
+            found = 1;
+            }
         }
 
     }
@@ -213,46 +226,22 @@ void search_disp_name(AddressBook *book, char string[],int size)
      
 }
 
-void search_disp_phone(AddressBook *book, char string[],int size)
-{
-    int fount=0;
-    printf("-----------------------------------------------");
-    printf("\n|%5s|%20s |%12s| |%30s|\n", "Sr.no","Name", "Phone", "Email");
-    printf("-----------------------------------------------");
 
-    for(int i = 0; i < book->contactCount; i++)
-    {
-        if(strstr(book->contacts.name[i],string) != NULL)
-        {
-            printf("|%5.4d| |%20.20s| |%12.12s| |%30.30s|\n", i + 1, book->contacts[i].name, book->contacts[i].phone, book->contacts[i].email);
-            found = 1;
-        }
-
-    }
-    if(found == 0)
-    {
-        printf("Contact not found");
-    }
-     
-}
 void searchContact(AddressBook *search)
 {
     int op = 0;
-    printf("Select the option to search by");
-    printf("1. Search by Name");
-    printf("2. Search by Phone no");
-    printf("3. Search by Email id");
-    scanf("%d",&op);
-    switch(op)
-    {
-        case 1 :
-        char temp_name[50];
-        search_disp_name(&search,temp_name,50);
-        break;
 
-        case 2 :
-        char temp_phone[10];
-        search_disp_name(&search,temp_phone,10);
+    printf("Select the option to search by\n");
+    printf("1. Search by Name\n");
+    printf("2. Search by Phone no\n");
+    printf("3. Search by Email id\n");
 
-    }
+    scanf("%d", &op);
+
+    char temp[50];
+
+    printf("Enter the search string: ");
+    scanf(" %[^\n]", temp);
+
+    search_disp(search, temp, 50, op);
 }
